b12cd5d1ba895e816baf15f22c15b7954315b6da
Manuel F. Dolz <dolzm@uji.es>
2021-09-19 13:18:43 +0200

    changes

--- Makefile
+++ Makefile
@@ -7,8 +7,8 @@
 
 CC       = gcc
 CLINKER  = gcc
-CFLAGS   = -fopenmp -ftree-vectorize -O3
-aOBJS    =
+CFLAGS   = -fopenmp -ftree-vectorize -O3 -DFP32
+# OBJS    =
 
 UNAME    = $(shell uname -m)
 
@@ -18,25 +18,30 @@ ifeq ($(UNAME), x86_64)
     OPTFLAGS = -L${MKLROOT}/lib/intel64_lin -Wl,--no-as-needed -lmkl_avx512 -lmkl_def -lmkl_intel_ilp64 -lmkl_intel_thread -lmkl_core -liomp5 -lpthread -lm -ldl
     #OPTFLAGS = -L${MKLROOT}/lib/intel64_lin -Wl,--no-as-needed -lmkl_intel_ilp64 -lmkl_sequential -lmkl_core -lpthread -lm -ldl
     INCLUDE  = -m64  -I"${MKLROOT}/include"
-    DTYPE    = -DFP32
-    OBJS    += conv_winograd_nchw_fp32.o \
-               conv_winograd_3x3_2x2_nchw_avx_fp32.o \
-               conv_winograd_2x2_3x3_nchw_avx_fp32.o \
-               conv_winograd_4x4_3x3_nchw_avx_fp32.o \
-               conv_winograd_2x2_5x5_nchw_avx_fp32.o
+    OBJS    += conv_winograd_3x3_2x2_sse_fp32_nchw.o \
+               conv_winograd_2x2_3x3_sse_fp32_nchw.o \
+               conv_winograd_4x4_3x3_sse_fp32_nchw.o \
+               conv_winograd_2x2_5x5_sse_fp32_nchw.o \
+               conv_winograd_3x3_2x2_sse_fp32_nhwc.o \
+               conv_winograd_2x2_3x3_sse_fp32_nhwc.o \
+               conv_winograd_4x4_3x3_sse_fp32_nhwc.o \
+               conv_winograd_2x2_5x5_sse_fp32_nhwc.o \
+               conv_winograd_fp32_nchw.o
   else
-    DTYPE    = -DFP32
     OPTFLAGS = -lm -lgomp
     OBJS    += conv_winograd_nchw_fp32.o gemm.o
   endif
 else ifeq ($(UNAME), aarch64)
-    DTYPE    = -DFP32
     FLAGS    = -DARM_NEON -DEXTERN_CBLAS
     OPTFLAGS = -L/home/dolzm/install/blis/lib -lblis -lgomp
-    OBJS    += conv_winograd_3x3_2x2_nchw_neon_fp32.o \
-               conv_winograd_2x2_3x3_nchw_neon_fp32.o \
-               conv_winograd_4x4_3x3_nchw_neon_fp32.o \
-               conv_winograd_2x2_5x5_nchw_neon_fp32.o
+    OBJS    += conv_winograd_3x3_2x2_neon_fp32_nchw.o \
+               conv_winograd_2x2_3x3_neon_fp32_nchw.o \
+               conv_winograd_4x4_3x3_neon_fp32_nchw.o \
+               conv_winograd_2x2_5x5_neon_fp32_nchw.o \
+               conv_winograd_3x3_2x2_neon_fp32_nhwc.o \
+               conv_winograd_2x2_3x3_neon_fp32_nhwc.o \
+               conv_winograd_4x4_3x3_neon_fp32_nhwc.o \
+               conv_winograd_2x2_5x5_neon_fp32_nhwc.o
 endif
 
 
@@ -50,18 +55,22 @@ default: $(LIBCONVWINOGRAD) $(WINOGRADDRIVER)
 $(LIBCONVWINOGRAD): $(OBJS)
 	$(CLINKER) $(OBJS) $(OPTFLAGS) -shared -o $@
 
-#-----------------------------------
+# #-----------------------------------
 
 $(WINOGRADDRIVER): test_winograd.c $(LIBCONVWINOGRAD) sutils.o
-	$(CC) $(DTYPE) -DARCH=$(UNAME) $^ $(OPTFLAGS) -o $@
-#	$(CC) $(DTYPE) $^ $(OPTFLAGS) -o $@
+	$(CC) -DFP32 $^ $(OPTFLAGS) -o $@
 
-#-----------------------------------
+# #-----------------------------------
 
-.c.o:
-	$(CC) $(CFLAGS) $(FLAGS) $(INCLUDE) $(DTYPE) -c $*.c
+%_nchw.o: %.c
+	$(CC) $(CFLAGS) -DTENSOR_FORMAT_NCHW $(FLAGS) $(INCLUDE) $(DTYPE) -c $*.c -o $@
 
-#-----------------------------------
+# #-----------------------------------
+
+%_nhwc.o: %.c
+	$(CC) $(CFLAGS) -DTENSOR_FORMAT_NHWC $(FLAGS) $(INCLUDE) $(DTYPE) -c $*.c -o $@
+
+# #-----------------------------------
 
 clean:
 	rm -f *.o $(LIBCONVWINOGRAD) $(WINOGRADDRIVER)
--- test_winograd.c
+++ test_winograd.c
@@ -41,32 +41,27 @@
 #define dabs(a)      ( (a) > 0.0 ? (a) : -(a) )
 #define min(a,b)     ( (a) > (b) ? (b) : (a) )
 
-#define Drow(a1,a2,a3,a4)  D[ (a1)*(ldD1)+(a2)*(ldD2)+(a3)*(ldD3)+(a4) ]
-#define Frow(a1,a2,a3,a4)  F[ (a1)*(ldF1)+(a2)*(ldF2)+(a3)*(ldF3)+(a4) ]
-#define Yrow(a1,a2,a3,a4)  Y[ (a1)*(ldY1)+(a2)*(ldY2)+(a3)*(ldY3)+(a4) ]
-#define Ygrow(a1,a2,a3,a4) Yg[ (a1)*(ldY1)+(a2)*(ldY2)+(a3)*(ldY3)+(a4) ]
-#define Urow(a1,a2,a3,a4)  U[ (a1)*(ldU1)+(a2)*(ldU2)+(a3)*(ldU3)+(a4) ]
-#define Vrow(a1,a2,a3,a4)  V[ (a1)*(ldV1)+(a2)*(ldV2)+(a3)*(ldV3)+(a4) ]
-#define Mrow(a1,a2,a3,a4)  M[ (a1)*(ldM1)+(a2)*(ldM2)+(a3)*(ldM3)+(a4) ]
+#define Drow_nchw(a1,a2,a3,a4)  D[ (a1)*(ldD1)+(a2)*(ldD2)+(a3)*(ldD3)+(a4) ]
+#define Frow_nchw(a1,a2,a3,a4)  F[ (a1)*(ldF1)+(a2)*(ldF2)+(a3)*(ldF3)+(a4) ]
+#define Yrow_nchw(a1,a2,a3,a4)  Y[ (a1)*(ldY1)+(a2)*(ldY2)+(a3)*(ldY3)+(a4) ]
+#define Ygrow_nchw(a1,a2,a3,a4) Yg[ (a1)*(ldY1)+(a2)*(ldY2)+(a3)*(ldY3)+(a4) ]
 
-#define Acol(a1,a2)  A[ (a2)*(ldA)+(a1) ]
-#define Bcol(a1,a2)  B[ (a2)*(ldB)+(a1) ]
-#define Ccol(a1,a2)  C[ (a2)*(ldC)+(a1) ]
-#define Cgcol(a1,a2) Cg[ (a2)*(ldC)+(a1) ]
-#define Arow(a1,a2)  A[ (a1)*(ldA)+(a2) ]
-#define Brow(a1,a2)  B[ (a1)*(ldB)+(a2) ]
-#define Crow(a1,a2)  C[ (a1)*(ldC)+(a2) ]
-#define Cgrow(a1,a2) Cg[ (a1)*(ldC)+(a2) ]
+#define Drow_nhwc(a1,a2,a3,a4)  D[ (a1)*(ldD1)+(a3)*(ldD2)+(a4)*(ldD3)+(a2) ]
+#define Frow_nhwc(a1,a2,a3,a4)  F[ (a2)*(ldF1)+(a3)*(ldF2)+(a4)*(ldF3)+(a1) ]
+#define Yrow_nhwc(a1,a2,a3,a4)  Y[ (a1)*(ldY1)+(a3)*(ldY2)+(a4)*(ldY3)+(a2) ]
+#define Ygrow_nhwc(a1,a2,a3,a4) Yg[ (a1)*(ldY1)+(a3)*(ldY2)+(a4)*(ldY3)+(a2) ]
 
 extern int    print_tensor4D( char *, int, int, int, int, DTYPE *, int, int, int );
 extern int    print_matrix( char *, char, int, int, DTYPE *, int );
 extern int    generate_tensor4D( int, int, int, int, DTYPE *, int, int, int );
 extern double dclock();
 
-#if ARCH == x86_64
-#define VARIANT _avx_
-#elif ARCH == aarch64
-#define VARIANT _neon_
+#if __x86_64__ && __LP64__
+#define VARIANT sse
+#elif __aarch64__ && __LP64__
+#define VARIANT neon
+#elif __riscv && __riscv_xlen==64
+#define VARIANT riscv
 #endif
 
 #define CONV_ARGS    int m, int r, int n, int k, int c, \
@@ -91,21 +86,27 @@ extern double dclock();
                     'F', 'F', NULL, NULL, NULL, NULL
 //                     NULL, NULL, NULL, NULL, U,  V, M, \
 
-#define DECL_FUNC2(v, a) conv_winograd_ ## v ## _nchw ## a ## fp32(CONV_ARGS)
-#define DECL_FUNC(v, a) DECL_FUNC2(v, a)
-
-#ifdef ARCH
-#define CALL_FUNC2(v, a) conv_winograd_ ## v ## _nchw ## a ## fp32(CONV_PARAMS(v))
-#define CALL_FUNC(v, a) CALL_FUNC2(v, a)
-extern void DECL_FUNC(3x3_2x2, VARIANT);
-extern void DECL_FUNC(2x2_3x3, VARIANT);
-extern void DECL_FUNC(4x4_3x3, VARIANT);
-extern void DECL_FUNC(2x2_5x5, VARIANT);
+#ifdef VARIANT
+#define DECL_FUNC2(v, a, f) conv_winograd_ ## v ## _ ## a ## _fp32_ ## f (CONV_ARGS)
+#define DECL_FUNC(v, a, f) DECL_FUNC2(v, a, f)
+#define CALL_FUNC2(v, a, f) conv_winograd_ ## v ## _ ## a ## _fp32_ ## f (CONV_PARAMS(v))
+#define CALL_FUNC(v, a, f) CALL_FUNC2(v, a, f)
+extern void DECL_FUNC(3x3_2x2, VARIANT, nchw);
+extern void DECL_FUNC(2x2_3x3, VARIANT, nchw);
+extern void DECL_FUNC(4x4_3x3, VARIANT, nchw);
+extern void DECL_FUNC(2x2_5x5, VARIANT, nchw);
+extern void DECL_FUNC(3x3_2x2, VARIANT, nhwc);
+extern void DECL_FUNC(2x2_3x3, VARIANT, nhwc);
+extern void DECL_FUNC(4x4_3x3, VARIANT, nhwc);
+extern void DECL_FUNC(2x2_5x5, VARIANT, nhwc);
 #else
-#define CALL_FUNC2(v, a) conv_winograd_nchw_fp32(CONV_PARAMS(v))
-#define CALL_FUNC(v, a) CALL_FUNC2(v, a)
-extern void conv_winograd_nchw_fp32(CONV_ARGS);
+#define CALL_FUNC2(v, a, f) conv_winograd_fp32_ ## f ## (CONV_PARAMS(v))
+#define CALL_FUNC(v, a, f) CALL_FUNC2(v, a, f)
+extern void conv_winograd_fp32(CONV_ARGS);
 #endif
+
+#define NCHW 0
+#define NHWC 1
 
 void convDirect( int, int, int, 
                  int, int, 
@@ -113,7 +114,7 @@ void convDirect( int, int, int,
                  int, int, 
                  DTYPE *, int, int, int, 
                  DTYPE *, int, int, int, 
-                 DTYPE *, int, int, int );
+                 DTYPE *, int, int, int, int );
 
 int main(int argc, char *argv[])
 {
@@ -121,7 +122,7 @@ int main(int argc, char *argv[])
   char* variant;
   DTYPE *D, *F, *Y, *Yg, *U, *V, *M;
   double t1, t2, time, tmin, error, nrm, tmp, errorthd, flops, GFLOPS;
-  int    m, t,
+  int    m, t, tmin_,
          nmin,  nmax,  nstep,
          kmin,  kmax,  kstep,
          cmin,  cmax,  cstep,
@@ -130,6 +131,7 @@ int main(int argc, char *argv[])
          rmin,  rmax,  rstep,
          smin,  smax,  sstep,
          prmax, psmax, ret,
+         tformat, tformatmin, tformatmax,
          n, k, c,
          h, w,
          r, s,
@@ -259,18 +261,19 @@ int main(int argc, char *argv[])
   tmin   = atof(argv[30]);
   test   = argv[31][0];
 
-  printf("# =================================================================================================");
+  printf("# =======================================================================================================");
   if ( test=='T' ) printf("======="); printf("\n");
   printf("# Driver for the evaluation of Winograd\n");
-  printf("# =================================================================================================");
+  printf("# =======================================================================================================");
   if ( test=='T' ) printf("======="); printf("\n");
-  printf("#    variant     n     k     c     h     w    kh    kw  vpad  hpad         Time    GFLOPS     Error");
+  printf("#    variant     n     k     c     h     w    kh    kw  vpad  hpad  format       Time    GFLOPS     Error");
   if ( test=='T' ) printf(" Status"); printf("\n");
   
   // Allocate space for data 
   // printf("# -->Allocate data\n"); fflush(stdout);
+  tformatmin=0; tformatmax=2;
   m = 2; t = 6;
-  int tmin_ = 4;
+  tmin_ = 4;
 
   homax = floor(((double) hmax + 2 * vpaddingmax - rmin) / 1) + 1;
   womax = floor(((double) wmax + 2 * hpaddingmax - smin) / 1) + 1;
@@ -306,6 +309,7 @@ int main(int argc, char *argv[])
   // for ( s=smin; s<=smax; s+=sstep ){
   for ( vpadding=vpaddingmin; vpadding<=vpaddingmax; vpadding+=vpaddingstep ){
   for ( hpadding=hpaddingmin; hpadding<=hpaddingmax; hpadding+=hpaddingstep ){
+  for ( tformat=tformatmin; tformat<tformatmax; tformat+=1 ){
     s = r;
     //hpadding = vpadding;
     // Generate random data
@@ -313,25 +317,49 @@ int main(int argc, char *argv[])
     ho = floor(((double) h + 2 * vpadding - r) / 1) + 1;
     wo = floor(((double) w + 2 * hpadding - s) / 1) + 1;
 
-    ldD3 = w;
-    ldD2 = h*ldD3;
-    ldD1 = c*ldD2;
+    if ( tformat == NCHW ) {
+      // NCHW
+      ldD3 = w;
+      ldD2 = h*ldD3;
+      ldD1 = c*ldD2;
 
-    ldF3 = s;
-    ldF2 = r*ldF3;
-    ldF1 = c*ldF2;
+      ldF3 = s;
+      ldF2 = r*ldF3;
+      ldF1 = c*ldF2;
 
-    ldY3 = wo;
-    ldY2 = ho*ldY3;
-    ldY1 = k*ldY2;
+      ldY3 = wo;
+      ldY2 = ho*ldY3;
+      ldY1 = k*ldY2;
 
-    generate_tensor4D( n, c, h, w, D, ldD1, ldD2, ldD3 );
-    generate_tensor4D( k, c, r, s, F, ldF1, ldF2, ldF3 );
+      generate_tensor4D( n, c, h, w, D, ldD1, ldD2, ldD3 );
+      generate_tensor4D( k, c, r, s, F, ldF1, ldF2, ldF3 );
+    } else {
+      // NHWC
+      ldD3 = c;
+      ldD2 = w*ldD3;
+      ldD1 = h*ldD2;
+
+      ldF3 = k;
+      ldF2 = s*ldF3;
+      ldF1 = r*ldF2;
+
+      ldY3 = k;
+      ldY2 = wo*ldY3;
+      ldY1 = ho*ldY2;
+
+      generate_tensor4D( n, h, w, c, D, ldD1, ldD2, ldD3 );
+      generate_tensor4D( c, r, s, k, F, ldF1, ldF2, ldF3 );
+   }
 
     // Print data
     if ( visual == 1 ){
-      print_tensor4D( "D", n, c, h, w, D, ldD1, ldD2, ldD3 );
-      print_tensor4D( "F", k, c, r, s, F, ldF1, ldF2, ldF3 );
+      if ( tformat == NCHW ) {
+        print_tensor4D( "D", n, c, h, w, D, ldD1, ldD2, ldD3 );
+        print_tensor4D( "F", k, c, r, s, F, ldF1, ldF2, ldF3 );
+      } else {
+        print_tensor4D( "D", n, h, w, c, D, ldD1, ldD2, ldD3 );
+        print_tensor4D( "F", c, r, s, k, F, ldF1, ldF2, ldF3 );
+      }
     }
 
     // Set result to zeros
@@ -339,8 +367,12 @@ int main(int argc, char *argv[])
     for ( ik=0; ik<k; ik++ )
     for ( ih=0; ih<ho; ih++ )
     for ( iw=0; iw<wo; iw++ ) {
-        Yrow(in,ik,ih,iw)  = 0.0;
-        Ygrow(in,ik,ih,iw) = 0.0;
+        if ( tformat == NCHW ) Yrow_nchw(in,ik,ih,iw) = 0.0 ;
+        else                   Yrow_nhwc(in,ik,ih,iw) = 0.0 ;
+        if ( test=='T' ) {
+          if ( tformat == NCHW ) Ygrow_nchw(in,ik,ih,iw) = 0.0 ;
+          else                   Ygrow_nhwc(in,ik,ih,iw) = 0.0 ;
+        }
     }
 
     // printf("# -->Solve problem\n"); fflush(stdout);
@@ -352,14 +384,23 @@ int main(int argc, char *argv[])
       // Winograd
       if ( strcmp(variant,"WINGRD\0")==0 ) {
         if (r == 2 && s == 2){
-           m = 3; CALL_FUNC(3x3_2x2, VARIANT);
+           m = 3;
+           tformat == NCHW ?
+             CALL_FUNC(3x3_2x2, VARIANT, nchw) :
+             CALL_FUNC(3x3_2x2, VARIANT, nhwc);
         }
         else if (r == 3 && s == 3) {
         //   m = 2; CALL_FUNC(2x2_3x3, VARIANT);
-           m = 4; CALL_FUNC(4x4_3x3, VARIANT);
+           m = 4;
+           tformat == NCHW ?
+             CALL_FUNC(4x4_3x3, VARIANT, nchw) :
+             CALL_FUNC(4x4_3x3, VARIANT, nhwc);
         }
         else if (r == 5 && s == 5) {
-           m = 2; CALL_FUNC(2x2_5x5, VARIANT);
+           m = 2;
+           tformat == NCHW ?
+             CALL_FUNC(2x2_5x5, VARIANT, nchw) :
+             CALL_FUNC(2x2_5x5, VARIANT, nhwc);
         }
         else break;
       }
@@ -383,17 +424,26 @@ int main(int argc, char *argv[])
                   vpadding, hpadding, 
                   D,  ldD1, ldD2, ldD3, 
                   F,  ldF1, ldF2, ldF3, 
-                  Yg, ldY1, ldY2, ldY3 );
+                  Yg, ldY1, ldY2, ldY3,
+                  tformat );
+
       error = 0.0;
       nrm   = 0.0;
       for ( in=0; in<n; in++ )
       for ( ik=0; ik<k; ik++ )
       for ( ih=0; ih<ho; ih++ )
       for ( iw=0; iw<wo; iw++ ) {
-        tmp = (double) Ygrow(in,ik,ih,iw);
-	nrm += tmp*tmp;
-        tmp = (double) dabs(Yrow(in,ik,ih,iw)-Ygrow(in,ik,ih,iw)); 
-        error += tmp*tmp;
+        if ( tformat == NCHW ) {
+          tmp = (double) Ygrow_nchw(in,ik,ih,iw);
+          nrm += tmp*tmp;
+          tmp = (double) dabs(Yrow_nchw(in,ik,ih,iw)-Ygrow_nchw(in,ik,ih,iw));
+          error += tmp*tmp;
+        } else {
+          tmp = (double) Ygrow_nhwc(in,ik,ih,iw);
+          nrm += tmp*tmp;
+          tmp = (double) dabs(Yrow_nhwc(in,ik,ih,iw)-Ygrow_nhwc(in,ik,ih,iw));
+          error += tmp*tmp;
+        }
       }
       if ( nrm!=0.0 )
         error = sqrt(error) / sqrt(nrm);
@@ -405,8 +455,13 @@ int main(int argc, char *argv[])
 
     // Print results
     if ( visual == 1 ) {
-      print_tensor4D( "Yc", n, k, h, w, Y, ldY1, ldY2, ldY3 );
-      print_tensor4D( "Ycd", n, k, h, w, Yg, ldY1, ldY2, ldY3 );
+      if ( tformat == NCHW ) {
+        print_tensor4D( "Yc", n, k, h, w, Y, ldY1, ldY2, ldY3 );
+        print_tensor4D( "Ycd", n, k, h, w, Yg, ldY1, ldY2, ldY3 );
+      } else {
+        print_tensor4D( "Yc", n, h, w, k, Y, ldY1, ldY2, ldY3 );
+        print_tensor4D( "Ycd", n, h, w, k, Yg, ldY1, ldY2, ldY3 );
+      }
     }
 
     //printf("-->Results\n");
@@ -414,15 +469,15 @@ int main(int argc, char *argv[])
     flops   = 2.0 * n * k * c * h * w * r * s;
     GFLOPS  = flops / (1.0e+9 * time );
     //printf("   GFLOPs       = %12.6e     \n", GFLOPS  );
-    printf("      %6s %5d %5d %5d %5d %5d %5d %5d %5d %5d %12.2e %9.2e %9.2e", 
-                    variant, n, k, c, h, w, r, s, vpadding, hpadding, time, GFLOPS, error );
+    printf("      %6s %5d %5d %5d %5d %5d %5d %5d %5d %5d %7s %10.2e %9.2e %9.2e",
+                    variant, n, k, c, h, w, r, s, vpadding, hpadding, ( tformat == NCHW ) ? "NCHW" : "NHWC", time, GFLOPS, error );
     if ( error<errorthd )   
       printf("   [OK]");   
     else 
       printf(" ******");   
     printf("\n");
 
-  } } } } } } } }// }
+  } } } } } } } } }
   /* Free data */
   free(Y);
   free(D);
@@ -446,7 +501,8 @@ void convDirect( int n, int k, int c,
                  int vpadding, int hpadding, 
                  DTYPE *D, int ldD1, int ldD2, int ldD3,
 	         DTYPE *F, int ldF1, int ldF2, int ldF3,
-                 DTYPE *Yg, int ldY1, int ldY2, int ldY3 )
+                 DTYPE *Yg, int ldY1, int ldY2, int ldY3,
+                 int tformat )
 { 
   int     in, ik, ic,
           ih, iw,
@@ -472,8 +528,12 @@ void convDirect( int n, int k, int c,
     if (0 <= x_x && x_x < h) 
       for ( is=0;  is<s;   is++ ) {
         x_y = iw + is - hpadding;
-        if (0 <= x_y && x_y < w)
-           Ygrow(in,ik,ih,iw) += Drow(in,ic,x_x,x_y) * Frow(ik,ic,ir,is);
+        if (0 <= x_y && x_y < w) {
+           if ( tformat == NCHW )
+              Ygrow_nchw(in,ik,ih,iw) += Drow_nchw(in,ic,x_x,x_y) * Frow_nchw(ik,ic,ir,is);
+           else
+              Ygrow_nhwc(in,ik,ih,iw) += Drow_nhwc(in,ic,x_x,x_y) * Frow_nhwc(ik,ic,ir,is);
+        }
       }
   }
 }  
